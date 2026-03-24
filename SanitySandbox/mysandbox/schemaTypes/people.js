1f7f9e3e99a32791f971a57e55bc66a59bf218e8
filipemamador <filipeamador@outlook.com>
2026-03-24 23:54:10 +0100

    Start med sanity

---
+++ SanitySandbox/mysandbox/schemaTypes/people.js
@@ -0,0 +1,10 @@
+const people = {
+    name: "people",
+    title: "Personer",
+    type: "document",
+    fields: [
+        { name: "fullname", title: "Fullt navn", type: "string" },
+        { name: "image", title: "Bilde", type: "image" }
+    ]
+}
+export default people
